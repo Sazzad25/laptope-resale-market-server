bda9c60aac946966fff3ae6a385b29f53152190a
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-26 07:29:57 +0600

    added category

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,30 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mwoallb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const categoryCollection = client.db('LaptopResale').collection('category');
+
+        app.get('/category', async(req, res) => {
+            const query = {};
+            const options = await categoryCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
+
 
 app.get('/', async(req, res) =>{
     res.send("laptop resale server is running")
