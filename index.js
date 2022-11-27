fece75f4fde78b182828523913ed26aca125de49
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-28 00:26:02 +0600

    added jwt path

--- index.js
+++ index.js
@@ -50,6 +50,17 @@ async function run(){
             res.send(result);
         });
 
+        app.get('/jwt', async (req, res) =>{
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if(user){
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {expiresIn: '1hr'})
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken: ''})
+        });
+
         app.post('/buyers', async(req, res) =>{
             const user = req.body;
             const result = await buyersCollection.insertOne(user);
