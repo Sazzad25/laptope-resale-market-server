4db8819407e18a8ac5db1e00d00bf26aba010de1
unknown <mdsazzadhosentamim95@gmail.com>
2022-11-28 22:17:11 +0600

    added admin

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -40,6 +41,21 @@ async function run(){
 
         const bookingCollection = client.db('LaptopResale').collection('bookings');
         const buyersCollection = client.db('LaptopResale').collection('buyers');
+        const addproductCollection = client.db('LaptopResale').collection('addproduct');
+        const paymentsCollection = client.db('LaptopResale').collection('payments');
+
+        const verifyAdmin = async (req, res, next) =>{
+            const decodedEmail = req.decoded.email;
+            const query = {email: decodedEmail};
+            const user = await buyersCollection.findOne(query);
+
+            if(user?.role !== 'admin'){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            next();
+        }
+
+
 
         app.get('/category', async(req, res) => {
             const query = {};
@@ -65,6 +81,13 @@ async function run(){
             const query = {email: email};
             const bookings = await bookingCollection.find(query).toArray();
             res.send(bookings);
+        });
+
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id) };
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
         })
 
         app.post('/bookings', async(req, res) =>{
@@ -72,6 +95,38 @@ async function run(){
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
         });
+
+        app.post('/create-payment-intent', async (req, res) =>{
+            const booking = req.body;
+            const resale_price = booking.price;
+            const amount = resale_price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+              });
+        });
+
+        app.post('/payments', async (req, res) =>{
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = {_id: ObjectId(id)}
+            const updateDoc = {
+                $set: {
+                    paid: true,
+                    transaction: payment.transactionId
+                }
+            }
+            const updatedResult = await bookingCollection.updateOne(filter, updateDoc)
+            res.send(updatedResult);
+        })
 
         app.get('/jwt', async (req, res) =>{
             const email = req.query.email;
@@ -90,11 +145,53 @@ async function run(){
             res.send(users);
         })
 
+        app.get('/buyers/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email}
+            const user = await buyersCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin'});
+        })
+
         app.post('/buyers', async(req, res) =>{
             const user = req.body;
             const result = await buyersCollection.insertOne(user);
             res.send(result);
+        });
+
+        app.put('/buyers/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
+           
+
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)}
+            const options = { upsert: true};
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await buyersCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+        app.get('/addproduct', verifyJWT, verifyAdmin, async(req, res) =>{
+            const query = {};
+            const result = await addproductCollection.find(query).toArray();
+            res.send(result);
         })
+
+        app.post('/addproduct', verifyJWT, verifyAdmin, async(req, res) =>{
+            const addproducts = req.body;
+            const result = await addproductCollection.insertOne(addproducts);
+            res.send(result);
+        });
+
+        app.delete('/addproduct/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const result = await addproductCollection.deleteOne(filter);
+            res.send(result);
+        })
+
 
         // app.get('/product/:id', async(req, res) => {
         //     const id = req.params.id;
