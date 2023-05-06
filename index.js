b78fbeabdd99be4aceee47d609ada9d2fe10cb54
unknown <mdsazzadhosentamim95@gmail.com>
2023-05-06 22:19:48 +0600

    data fetching

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
+// mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mwoallb.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
